a199595f5996bcecd503c1a4219ac147f870b8e4
Juniorsuleiman <153205065+Juniorsuleiman@users.noreply.github.com>
2023-12-16 16:26:30 +0000

    Update Aphids_response_to_odours_Suleiman_Mustapha.py

--- Aphids_response_to_odours_Suleiman_Mustapha.py
+++ Aphids_response_to_odours_Suleiman_Mustapha.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Using biopython to explore the olfactory response of the cabbage aphids to different volatile cues 
-
-# In this experiment, different volatile cues were collected from cabbage plant which serves as a sustaible host plant for the cabbage aphids. These volatile cues ranged from plants without any infestation (HC = healthy cabbage), aphids infested cabbage plants (AC) as conspecific cues, diamondback moth larvae infested cabbage (DC) as heterospecific cues and a solvent control (CN). Aphids were exposed to the various odour cues in a four-choice laboratory olfactometer bioassay, and the time spent in each of the arms of the olfactomer was recorded (time spent = min). Using python, I demonstrate how the collected data could be explored, visualized, manipulated and also analyzed. This work is currently still in progress so data should be treated with caution. 
-
 # # Importing and loading the data set
 
 # In[2]:
